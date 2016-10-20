a9fc905845d613c1624d0295470eb5c9f10aaef9
ctkremer <colin.kremer@yale.edu>
2016-10-20 10:31:46 -0400

    Create folder to save package data and examples

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Species biomasses in communities X and Y
+#'
+#' A small example dataset containing the biomass and names of 30 species in two plant communities.
+#'
+#' @format A data frame with 30 rows and 3 variables:
+#' \describe{
+#'   \item{species}{list of species ids}
+#'   \item{biomassX}{mass of species in community X, in grams/m^2}
+#'   \item{biomassY}{mass of species in community Y, in grams/m^2}
+#' }
+#' @source Placeholder
+"biomass"
