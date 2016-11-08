93525176e501e86d8a0ed38b105492fd32fb18af
ctkremer <colin.kremer@yale.edu>
2016-11-07 21:42:43 -0500

    Updated to-do list

--- TODO.R
+++ TODO.R
@@ -8,9 +8,15 @@
 
 # make default aspect ratio of plots 1; i.e. square
 # write complete documentation
-# add sample data sets for examples
 # add 2-part partition (BEF style) option to get.dist.mat() function
 # come up with a better/faster/more efficient way of performing Jaccard computations
 # add examples to pairwise.price() function's documentation
-# finish tidying code in leap.zig suite of functions
 # work on examples in documentation and vignette.
+
+#     * Shift current plotting code into true object-oriented approach, defining classes for bef, cafe, & price data sets, and associated plotting and testing methods?
+#     * try to reduce the length and redundancy of plotting functions
+#     * Generalize code to allow for facetting of ggplots over multiple levels of a treatment variable, rather than the current approach of pasting together complete graphs
+#     * Set up an option to draw individual vector paths for specific pairs of communities.
+#     * Add nonparametric tests to `test.partitions()`
+#     * Extend tools & existing code stubs to account for more complex experimental designs, including multiple treatments, interaction effects, etc.
+
