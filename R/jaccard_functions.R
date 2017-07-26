140a9e0e6d2e633ba35b67acc98be420b12ded44
ctkremer <colin.kremer@yale.edu>
2017-07-26 14:04:09 -0400

    Change made to prevent error caused by class 'rowwise_df' on res, which prevented unnest() from functioning. See https://stackoverflow.com/questions/29762393/how-does-one-stop-using-rowwise-in-dplyr

--- R/jaccard_functions.R
+++ R/jaccard_functions.R
@@ -51,7 +51,7 @@ pairwise.jaccard<-function(x,species='Species',func='Function'){
     names(res)[1:length(gps)] <- paste(names(res[1:length(gps)]),"x",sep=".") 
 
     # expand the tibble returned by do()
-    res<-tidyr::unnest(res)
+    res<-tidyr::unnest(ungroup(res))
     
     # fix labels of comparison community's grouping variables
     locs<-which(names(res) %in% gps)
