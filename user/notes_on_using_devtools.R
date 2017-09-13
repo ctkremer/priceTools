d16d49df96d2d67d0eb4f6274aac2aae03c2e497
ctkremer <colin.kremer@yale.edu>
2017-09-13 10:13:54 -0400

    note file created

---
+++ user/notes_on_using_devtools.R
@@ -0,0 +1,13 @@
+
+#Useful devtools commands to remember:
+
+# at initial set-up  
+devtools::use_readme_rmd()
+devtools::use_build_ignore("NEWS.md")
+devtools::use_build_ignore("TODO.R")
+
+# during active development
+devtools::load_all()  # re-load all files in /R
+
+
+ggplot2/NEWS.md 
